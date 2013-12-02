25b2c86a88e4858418f895128256a6b54243d7fe
shirst200 <shirst200@caledonian.ac.uk>
2013-12-02 09:29:41 +0000

    Attempt with lists

---
+++ Lab1/ResourceHolder.cpp
@@ -0,0 +1,33 @@
+#include "ResourceHolder.h"
+
+Texture ResourceHolder::GetTexture(LPCSTR fileName)
+{
+	Texture* tixture=new Texture(NULL);
+	list<Texture>::iterator ListItem;
+	for (ListItem = texture.begin();ListItem != texture.end();++ListItem)
+	{
+		tixture = ListItem;
+		if(ListItem. .GetName()!=fileName)
+		{
+			return texture[i];
+		}
+	}
+	texture.assign(new Texture(new const CHAR('s')),texture.end());
+	Texture texture[i] = new Texture(fileName);
+	return texture[i];
+}
+
+bool ResourceHolder::DeleteTexture(LPCSTR fileName)
+{
+
+}
+
+ID3D10Effect* ResourceHolder::GetEffect(LPCSTR fileName)
+{
+
+}
+
+bool ResourceHolder::DeleteEffect(LPCSTR fileName)
+{
+
+}
---
+++ Lab1/ResourceHolder.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../GameApplication/Texture.h"
+#include "D3D10.h"
+#include <list>
+#include <string>
+using namespace std;
+
+class ResourceHolder
+{
+public:
+	ResourceHolder();
+	~ResourceHolder();
+
+	Texture GetTexture(LPCSTR fileName);
+	bool DeleteTexture(LPCSTR fileName);
+
+	ID3D10Effect* GetEffect(LPCSTR fileName);
+	bool DeleteEffect(LPCSTR fileName);
+
+private:
+	std::list<Texture> texture;
+	ID3D10Effect *held_Effect[10];
+	
+};
