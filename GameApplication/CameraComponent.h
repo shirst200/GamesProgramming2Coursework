74105d14d82df1582d84809fdbd7b28c88ce8bba
shirst200 <shirst200@caledonian.ac.uk>
2013-12-02 12:00:13 +0000

    i am the fixer of gubbins

--- GameApplication/CameraComponent.h
+++ GameApplication/CameraComponent.h
@@ -1,108 +1,111 @@
+#pragma once
+
 #include "GameComponent.h"
 
 #include <windows.h>
 #define _XM_NO_INTRINSICS_
 #include <xnamath.h>
 
-
 class CameraComponent:public GameComponent
 {
 public:
-	CameraComponent()																						//constructor for the class
-	{
-		m_LookAt=XMFLOAT3(0.0f,0.0f,0.0f);
-		m_Up=XMFLOAT3(0.0f,1.0f,0.0f);
-		m_FOV=XM_PI/4;
-		m_AspectRatio=800.0f/640.0f;
-		m_NearClip=0.1f;
-		m_FarClip=1000.0f;
-		m_View=XMMatrixIdentity();
-		m_Projection=XMMatrixIdentity();
-		m_Name="Camera";
-	};			
+        CameraComponent()
+        {
+                m_Name="Camera";
+                m_LookAt=XMFLOAT3(0.0f,0.0f,0.0f);
+                m_Up=XMFLOAT3(0.0f,1.0f,0.0f);
+                m_View=XMMatrixIdentity();
+                m_Projection=XMMatrixIdentity();
+                m_FOV=XM_PI/4;
+                m_AspectRatio=800.0f/640.0f;
+                m_Near=0.1f;
+                m_Far=1000.0f;
+        };
 
-	~CameraComponent(){};																					//deconstructor for the class
+        ~CameraComponent()
+        {
+        }
 
-	void setLook(float x, float y, float z)																	//set function for the point the camera is looking at
-	{
-		m_LookAt = XMFLOAT3(x,y,z);
-	};				
 
-	void setUp(float x, float y, float z)																	//set function for the up direction of the camera
-	{
-		m_Up = XMFLOAT3(x,y,z);
-	};	
+        void update();
 
-	void setFOV(float fov)																					//set function for the field of the view of the camera
-	{
-		m_FOV = fov;
-	};		
+        void setLook(float x,float y,float z)
+        {
+                m_LookAt=XMFLOAT3(x,y,z);
+        };
 
-	void setAspectRatio(float ratio)																		//set function for the aspect ratio of the camera
-	{
-		m_AspectRatio = ratio;
-	};	
+        void setUp(float x,float y,float z)
+        {
+                m_Up=XMFLOAT3(x,y,z);
+        };
 
-	void setNearClip(float near)																			//set function for the near clip of the camera
-	{
-		m_NearClip = near;
-	};
+        void setFOV(float fov)
+        {
+                m_FOV=fov;
+        };
 
-	void setFarClip(float far)																				//set function for the far clip of the camera
-	{
-		m_FarClip = far;
-	};		
+        void setAspectRatio(float aspectRatio)
+        {
+                m_AspectRatio=aspectRatio;
+        };
 
-	XMFLOAT3& getLook()																						//get function for the point the camera is looking at
-	{
-		return m_LookAt;
-	};				
-	
-	XMFLOAT3& getUp()																						//get function for the up direction of the camera
-	{
-		return m_Up;
-	};		
+        void setNearClip(float nearClip)
+        {
+                m_Near=nearClip;
+        };
 
-	float getFOV()																							//get function for the field of view of the camera
-	{
-		return m_FOV;
-	};	
+        void setFarClip(float farClip)
+        {
+                m_Far=farClip;
+        };
 
-	float getAspectRatio()																					//get function for the aspect ratio of the camera
-	{
-		return m_AspectRatio;
-	};			
+        XMFLOAT3& getLookAt()
+        {
+                return m_LookAt;
+        };
 
-	float getNearClip()																						//get function for the near clip of the camera
-	{
-		return m_NearClip;
-	};	
+        XMFLOAT3& getUp()
+        {
+                return m_Up;
+        };
 
-	float getFarClip()																						//get function for the far clip of the camera
-	{
-		return m_FarClip;
-	};		
+        float getFOV()
+        {
+                return m_FOV;
+        };
 
-	XMMATRIX& getView()																						//get function for the view matrix
-	{
-		return m_View;
-	};	
+        float getAspectRatio()
+        {
+                return m_AspectRatio;
+        };
 
-	XMMATRIX& getProjection()																				//get function for the projection matrix
-	{
-		return m_Projection;
-	};		
+        float getNear()
+        {
+                return m_Near;
+        };
 
-	void update();																							//overridden from the base class
+        float getFar()
+        {
+                return m_Far;
+        };
 
+        XMMATRIX& getView()
+        {
+                return m_View;
+        };
+
+        XMMATRIX& getProjection()
+        {
+                return m_Projection;
+        };
 private:
-	XMFLOAT3 m_LookAt;																						//This represents the point the camera is looking at
-	XMFLOAT3 m_Up;																							//This represents the up direction of the camera
-	float m_FOV;																							//The field of view of the camera
-	float m_AspectRatio;																					//The aspect ratio of the camera
-	float m_NearClip;																						//The near clip of the camera
-	float m_FarClip;																						//The far clip of the camera
-	XMMATRIX m_View;																						//Represents a view matrix
-	XMMATRIX m_Projection;																					//Represents a projection matrix	
-																											//Name held in base class to manage camera component
+        XMFLOAT3 m_LookAt;
+        XMFLOAT3 m_Up;
+        float m_FOV;
+        float m_AspectRatio;
+        float m_Near;
+        float m_Far;
+
+        XMMATRIX m_View;
+        XMMATRIX m_Projection;
 };
