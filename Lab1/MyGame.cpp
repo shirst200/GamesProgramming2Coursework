a558661f67d7579a2bffa3528858203ab4b8c283
Ryan Coughlan <rcough200@caledonian.ac.uk>
2013-12-06 14:03:41 +0000

    Added level builder to MyGame

--- Lab1/MyGame.cpp
+++ Lab1/MyGame.cpp
@@ -2,44 +2,13 @@
 
 bool MyGame::initGame()
 {
-        CubeVisualComponent *pCube=new CubeVisualComponent();
-        pCube->create(m_pRenderer);
-        
-        Material *pMaterial=new Material();
-        pMaterial->loadEffect("Effects/Texture.fx",m_pRenderer);
-        pMaterial->loadDiffuseTexture("Textures/Spotlight.jpg",m_pRenderer);
-
-        GameObject *pTestObj=new GameObject();
-        pTestObj->setName("TestObject");
-        pTestObj->addComponent(pCube);
-        pTestObj->addComponent(pMaterial);
-
-        pCube->createVertexLayout(m_pRenderer);
-        
-        pTestObj->getTransform().setPosition(-2.0f,1.0f,1.0f);
-
-        m_GameObjectList.push_back(pTestObj);
-
-        CameraComponent *pCameraComp = new CameraComponent();
-        pCameraComp->setLook(0.0f,0.0f,0.0f);
-        pCameraComp->setFOV(m_GameOptionDesc.width/m_GameOptionDesc.height);
-        
-		GameObject *pCameraGO = new GameObject();
-        pCameraGO->setName("MainCamera");
-        pCameraGO->addComponent(pCameraComp);
-        setMainCamera(pCameraComp);
-        pCameraGO->getTransform().setPosition(0.0f,0.0f,-10.0f);
-
-        m_GameObjectList.push_back(pCameraGO);
-		CubeVisualComponent *pCube1=new CubeVisualComponent();
+	/* CubeVisualComponent *pCube1=new CubeVisualComponent();
         pCube1->create(m_pRenderer);
         
         Material *pMaterial1=new Material();
         pMaterial->loadEffect("Effects/Specular.fx",m_pRenderer);
 
-
         DirectionLightComponent * pDirLight=new DirectionLightComponent();
-
 
         GameObject * pTestObj2=new GameObject();
         pTestObj2->setName("TestObject2");
@@ -53,7 +22,47 @@ bool MyGame::initGame()
         pTestObj2->getTransform().setPosition(0.0f,1.0f,1.0f);
 
 
-        m_GameObjectList.push_back(pTestObj2);
+        m_GameObjectList.push_back(pTestObj2);*/
+
+        CameraComponent *pCameraComp = new CameraComponent();
+        pCameraComp->setLook(4.0f,0.0f,4.0f);
+        pCameraComp->setFOV(m_GameOptionDesc.width/m_GameOptionDesc.height);
+        
+		GameObject *pCameraGO = new GameObject();
+        pCameraGO->setName("MainCamera");
+        pCameraGO->addComponent(pCameraComp);
+        setMainCamera(pCameraComp);
+        pCameraGO->getTransform().setPosition(4.0f,10.0f,4.0f);
+
+        m_GameObjectList.push_back(pCameraGO);
+		
+		bool gridSpots[16] = {1,1,1,1,
+							1,0,0,1,
+							1,0,0,1,
+							1,1,1,1,};
+
+		for(int i=0;i<16;i++){
+			if(gridSpots[i])
+			{
+				CubeVisualComponent *pCube=new CubeVisualComponent();
+				pCube->create(m_pRenderer);
+        
+				Material *pMaterial=new Material();
+				pMaterial->loadEffect("Effects/Texture.fx",m_pRenderer);
+				pMaterial->loadDiffuseTexture("Textures/Spotlight.jpg",m_pRenderer);
+
+				GameObject *pTestObj=new GameObject();
+				pTestObj->setName("TestObject");
+				pTestObj->addComponent(pCube);
+				pTestObj->addComponent(pMaterial);
+
+				pCube->createVertexLayout(m_pRenderer);
+        
+				pTestObj->getTransform().setPosition(i%4, 1, i/4);
+
+				m_GameObjectList.push_back(pTestObj);
+			}
+		}
 
         return true;
 }
--- Lab1/MyGame.h
+++ Lab1/MyGame.h
@@ -9,4 +9,7 @@ public:
 	~MyGame(){};
 
 	bool initGame();
+
+private:
+        bool gridSpots[];
 };
