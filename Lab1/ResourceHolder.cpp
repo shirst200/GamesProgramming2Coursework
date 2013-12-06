2308c41d5fe613ad5c2f102520a87dbef506bf69
shirst200 <shirst200@caledonian.ac.uk>
2013-12-06 11:48:12 +0000

    Effect fully implemented but untested

--- Lab1/ResourceHolder.cpp
+++ Lab1/ResourceHolder.cpp
@@ -6,7 +6,9 @@ ID3D10Texture2D* ResourceHolder::GetTexture(LPCSTR fileName)
 	if(texture.find(fileName)==texture.end())
 	{
 		//If not found add and return the added value
-		texture.insert(fileName);
+		ID3D10ShaderResourceView * m_pBaseTextureMap;
+		D3DX10CreateShaderResourceViewFromFileA(m_pD3D10Device,fileName,NULL,NULL,&m_pBaseTextureMap,NULL);
+		//texture[fileName]=;
 		return texture.at(fileName);
 	}
 	//If found return
@@ -36,7 +38,34 @@ ID3D10Effect* ResourceHolder::GetEffect(LPCSTR fileName)
 	if(effect.find(fileName)==effect.end())
 	{
 		//If not found add and return it
-		effect.insert(fileName);
+		DWORD dwShaderFlags = D3D10_SHADER_ENABLE_STRICTNESS;
+#if defined( DEBUG ) || defined( _DEBUG )
+        dwShaderFlags |= D3D10_SHADER_DEBUG;
+#endif
+
+        ID3D10Blob * pErrorBuffer=NULL;
+        if (FAILED(D3DX10CreateEffectFromFileA(fileName,
+                NULL,
+                NULL,
+                "fx_4_0", //A string which specfies the effect profile to use, in this case fx_4_0(Shader model 4) - BMD
+                dwShaderFlags, //Shader flags, this can be used to add extra debug information to the shader - BMD
+                0,//Fx flags, effect compile flags set to zero - BMD
+			    m_pD3D10Device, //ID3D10Device*, the direct3D rendering device - BMD
+                NULL, //ID3D10EffectPool*, a pointer to an effect pool allows sharing of variables between effects - BMD
+                NULL, //ID3DX10ThreadPump*, a pointer to a thread pump this allows multithread access to shader resource - BMD
+                &m_pTempEffect, //ID3D10Effect**, a pointer to a memory address of the effect object. This will be initialised after this - BMD
+                &pErrorBuffer, //ID3D10Blob**, a pointer to a memory address of a blob object, this can be used to hold errors from the compilation - BMD
+                NULL )))//HRESULT*, a pointer to a the result of the compilation, this will be NULL - BMD
+        {
+                OutputDebugStringA((char*)pErrorBuffer->GetBufferPointer());
+                return false;
+        }
+		effect[fileName]=m_pTempEffect;
+		holder =(m_pTempEffect->GetTechniqueByName("Render"));
+
+        m_pWorldEffectVariable[fileName]=m_pTempEffect->GetVariableByName("matWorld")->AsMatrix();
+        m_pProjectionEffectVariable=m_pTempEffect->GetVariableByName("matProjection")->AsMatrix();
+        m_pViewEffectVariable=m_pTempEffect->GetVariableByName("matView")->AsMatrix();
 		return effect.at(fileName);
 	}
 	//If effect found return it
@@ -60,7 +89,7 @@ bool ResourceHolder::DeleteEffect(LPCSTR fileName)
 	return true;
 }
 
-ID3D10Mesh* ResourceHolder::GetMesh(LPCSTR fileName)
+/*ID3D10Mesh* ResourceHolder::GetMesh(LPCSTR fileName)
 {
 	//Searches for mesh
 	if(mesh.find(fileName)==mesh.end())
@@ -88,4 +117,4 @@ bool ResourceHolder::DeleteMesh(LPCSTR fileName)
 	}
 	//If deleted return true
 	return true;
-}
+}*/
--- Lab1/ResourceHolder.h
+++ Lab1/ResourceHolder.h
@@ -2,6 +2,7 @@
 
 //Includes files needed for the variables
 #include "D3D10.h"
+#include "D3DX10.h"
 #include <map>
 
 using namespace std;
@@ -22,12 +23,19 @@ public:
 	bool DeleteEffect(LPCSTR fileName);
 
 	//Mesh management
-	ID3D10Mesh* GetMesh(LPCSTR fileName);
-	bool DeleteMesh(LPCSTR fileName);
+	/*ID3D10Mesh* GetMesh(LPCSTR fileName);
+	bool DeleteMesh(LPCSTR fileName);*/
 
 private:
 	//Mapped variables
+	ID3D10Effect * m_pTempEffect;
+
+	map < LPCSTR, ID3D10EffectMatrixVariable*> m_pWorldEffectVariable;
+    ID3D10EffectMatrixVariable * m_pProjectionEffectVariable;
+    ID3D10EffectMatrixVariable * m_pViewEffectVariable;
+ID3D10EffectTechnique * holder;
+	ID3D10Device * m_pD3D10Device;
 	map < LPCSTR, ID3D10Texture2D* > texture;
 	map < LPCSTR, ID3D10Effect* > effect;
-	map < LPCSTR, ID3D10Mesh > mesh;
+	//map < LPCSTR, ID3D10Mesh > mesh;
 };
