d6183e996c033dee6ae47c6eb588019e53904dcd
shirst200 <shirst200@caledonian.ac.uk>
2013-12-12 10:06:57 +0000

    (stillRunnable)

--- D3D10Renderer/D3D10Renderer.h
+++ D3D10Renderer/D3D10Renderer.h
@@ -12,70 +12,77 @@
 #define _XM_NO_INTRINSICS_
 #include <xnamath.h>
 #include <queue>
+#include <vector>
 
 #include "Vertex.h"
-
 
 //D3D10Renderer implements the Renderer interface
 class D3D10Renderer:public IRenderer
 {
 public:
-	D3D10Renderer();
-	~D3D10Renderer();
-	//notice these have the same signature as the pure methods
-	//in the interface
-	bool init(void *pWindowHandle,bool fullScreen);
-	void clear(float r,float g,float b,float a);
-	void present();
-	void render();
+        D3D10Renderer();
+        ~D3D10Renderer();
+        //notice these have the same signature as the pure methods
+        //in the interface
+        bool init(void *pWindowHandle,bool fullScreen);
+        void clear(float r,float g,float b,float a);
+        void present();
+        void render();
 
-	ID3D10Effect * loadEffectFromMemory(const char *pMem);
-	ID3D10Effect * loadEffectFromFile(const char *pFilename);
+        ID3D10Effect * loadEffectFromMemory(const char *pMem);
+        ID3D10Effect * loadEffectFromFile(const char *pFilename);
 
-	ID3D10Buffer * createVertexBuffer(int size,Vertex *pVerts);
-	ID3D10Buffer * createIndexBuffer(int size,int *pIndices);
-	ID3D10InputLayout * createVertexLayout(ID3D10Effect * pEffect);
+        ID3D10Buffer * createVertexBuffer(int size,Vertex *pVerts);
+        ID3D10Buffer * createIndexBuffer(int size,int *pIndices);
+        ID3D10InputLayout * createVertexLayout(ID3D10Effect * pEffect);
 
-	void addToRenderQueue(GameObject *pObject);
-	void setProjection(XMMATRIX& projection)
-	{
-		m_Projection=projection;
-	};
+        ID3D10ShaderResourceView * loadTexture(const char *pFilename);
+        void addToRenderQueue(GameObject *pObject);
 
-	void setView(XMMATRIX& view)
-	{
-		m_View=view;
-	};
-		        void setAmbientColour(float r,float g,float b,float a);
-ID3D10ShaderResourceView * loadTexture(const char *pFilename);
+        void setProjection(XMMATRIX& projection)
+        {
+                m_Projection=projection;
+        };
+
+        void setView(XMMATRIX& view)
+        {
+                m_View=view;
+        };
+
+        void setAmbientLightColour(float r,float g,float b,float a)
+        {
+                m_AmbientLightColour=XMCOLOR(r,g,b,a);
+        };
+
 private:
-	bool createDevice(HWND pWindowHandle,int windowWidth, int windowHeight,
+        bool createDevice(HWND pWindowHandle,int windowWidth, int windowHeight,
 bool fullScreen);
-	bool createInitialRenderTarget(int windowWidth, int windowHeight);
+        bool createInitialRenderTarget(int windowWidth, int windowHeight);
 
+        void render(GameObject *pCurrentObject);
 private:
-	        XMCOLOR ambientLightColour;
+        typedef std::queue<GameObject*> RenderQueue;
+        
+        //D3D10 stuff
+        ID3D10Device * m_pD3D10Device;
+        IDXGISwapChain * m_pSwapChain;
+        ID3D10RenderTargetView * m_pRenderTargetView;
+        ID3D10DepthStencilView * m_pDepthStencelView;
+        ID3D10Texture2D *m_pDepthStencilTexture;
 
-	typedef std::queue<GameObject*> RenderQueue;
-	//D3D10 stuff
-	ID3D10Device * m_pD3D10Device;
-	IDXGISwapChain * m_pSwapChain;
-	ID3D10RenderTargetView * m_pRenderTargetView;
-	ID3D10DepthStencilView * m_pDepthStencelView;
-	ID3D10Texture2D *m_pDepthStencilTexture;
-	        ID3D10EffectVectorVariable * m_pDiffuseLightColour;
-        ID3D10EffectVectorVariable * m_pLightDirection;
-        ID3D10EffectVectorVariable * m_pSpecularLightColour;
-	//Vertex Layout
-	ID3D10InputLayout*      m_pDefaultVertexLayout;
-	//this will be used if we have no Effect
-	ID3D10Effect * m_pDefaultEffect;
-	ID3D10EffectTechnique * m_pDefaultTechnique;
+        //Vertex Layout
+        ID3D10InputLayout*      m_pDefaultVertexLayout;
+        //this will be used if we have no Effect
+        ID3D10Effect * m_pDefaultEffect;
+        ID3D10EffectTechnique * m_pDefaultTechnique;
 
-	RenderQueue m_RenderQueue;
+        RenderQueue m_RenderQueue;
 
-	XMMATRIX m_View;
-	XMMATRIX m_Projection;
-	        GameObject * m_pMainLight;
+        XMMATRIX m_View;
+        XMMATRIX m_Projection;
 
+        XMCOLOR m_AmbientLightColour;
+
+        GameObject * m_pMainCamera;
+        GameObject * m_pMainLight;
 };
